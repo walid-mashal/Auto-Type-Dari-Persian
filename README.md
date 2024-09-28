674241b7160110b876d1510d455be5c6f61e597f
walid-mashal <walidmashal4@gmail.com>
2024-09-28 09:03:32 +0430

    first commit

---
+++ README.md
@@ -0,0 +1,32 @@
+### Type Dari/Persian
+
+This javascript module is used to help web developers apply the functionality of typing Dari/Persian language without any need to change the system language.
+
+To Use: 
+
+	<input lang="dr" type="text"/>
+
+	<textarea lang="dr"></textarea>
+
+	<div contenteditable="true" lang="dr"></div>
+	
+
+and add a reference in your html file to the TypeDariPersian.js
+
+	<script type="text/javascript" src="path/to/folder/TypeDariPersian.js"></script>
+
+For this module to work perfectly the input/textarea element needs to have an attribute of lang="dr".
+
+This module will itself make the input box's text align right to left so that it is shown properly in the input box.
+
+
+<!-- ### Demo:
+
+![](https://github.com/walid-mashal/Type-Dari-Persian/blob/master/img/pashto_type_final.gif) -->
+
+
+### Keyboard Structure
+
+This module assumes that the keyboard structure of your computer is as of the following image.
+
+![keyboard layout](https://github.com/walid-mashal/Type-Dari-Persian/blob/master/img/dari-keyboard.png)
---
+++ js/TypeDariPersian.js
@@ -0,0 +1,85 @@
+﻿/*
+Auto Type Dari/Persian
+Version: 1.1
+This module enables typing Dari/Persian in web forms without changing system language settings.
+The layout is based on the Afghanistan Official Dari Keyboard Layout.
+*/
+
+let shiftIsPressed = false;
+let ctrlIsPressed = false;
+
+const dariKeyboard = {
+    dariCodeMap: {
+        '104': 'ا', 'shift+71': 'أ', '102': 'ب',  
+        'shift+72': 'آ', 'shift+76': 'ة', '106': 'ت', '101': 'ث', 
+        '91': 'ج', '112': 'ح', '105': 'ه', '111': 'خ', 'shift+80': '[', 
+        'shift+90': 'ئ', 'shift+86': 'ء', '93': 'چ', '110': 'د', '98': 'ذ', 
+        '118': 'ر', '99': 'ز', 'shift+67': 'ژ', 'shift+77': 'ؤ', '44': 'و', 
+        '115': 'س', '97': 'ش', '119': 'ص', '113': 'ض', 'shift+88': 'ط', 
+        '122': 'ظ', '117': 'ع', '121': 'غ', '116': 'ف', '114': 'ق', 
+        '59': 'ک', '39': 'گ', '108': 'م', '107': 'ن', '103': 'ل', '100': 'ی', 
+        'shift+68': 'ي', '32': ' ', '49': '۱', '50': '۲', '51': '۳', 
+        '52': '۴', '53': '۵', '54': '۶', '55': '۷', '56': '۸', '57': '۹', 
+        '48': '۰', ',': ',', '-': '-', 'shift+74' : 'ة', 'shift+70' : 'إ', 
+        'shift+90' : 'ك', 'shift+79' : ']', '120' : 'ط', '109' : 'پ'
+    },
+
+    init() {
+        const elements = document.querySelectorAll('input[type="text"][lang="ps-dr"], textarea[lang="ps-dr"], div[contenteditable="true"][lang="ps-dr"]');
+        elements.forEach((element) => this.bindDariTyping(element));
+    },
+
+    bindDariTyping(element) {
+        element.style.textAlign = 'right';
+        element.style.direction = 'rtl';
+
+        element.addEventListener('keypress', (e) => this.handleKeyPress(e, element));
+        element.addEventListener('keydown', this.handleKeyDown);
+        element.addEventListener('keyup', this.handleKeyUp);
+    },
+
+    handleKeyDown(e) {
+        if (e.key === 'Shift') shiftIsPressed = true;
+        if (e.ctrlKey) ctrlIsPressed = true;
+    },
+
+    handleKeyUp(e) {
+        if (e.key === 'Shift') shiftIsPressed = false;
+        if (!e.ctrlKey) ctrlIsPressed = false;
+    },
+
+    handleKeyPress(e, element) {
+        console.log("keycode...........",e.charCode,e.keyCode);
+        if (!ctrlIsPressed) {
+            const keyCode = e.charCode || e.keyCode;
+            const keyStr = shiftIsPressed ? 'shift+' + keyCode : keyCode.toString();
+            const dariChar = this.dariCodeMap[keyStr];
+
+            if (dariChar) {
+                this.insertTextAtCaret(element, dariChar);
+                e.preventDefault();
+            }
+        }
+    },
+
+    insertTextAtCaret(element, char) {
+        if (!element.isContentEditable) {
+            const { selectionStart, selectionEnd, value } = element;
+            element.value = value.slice(0, selectionStart) + char + value.slice(selectionEnd);
+            element.setSelectionRange(selectionStart + 1, selectionStart + 1);
+        } else {
+            const sel = window.getSelection();
+            const { anchorOffset, focusOffset } = sel;
+            const content = element.textContent;
+            element.textContent = content.slice(0, anchorOffset) + char + content.slice(focusOffset);
+
+            const range = document.createRange();
+            range.setStart(element.firstChild, anchorOffset + 1);
+            range.collapse(true);
+            sel.removeAllRanges();
+            sel.addRange(range);
+        }
+    }
+};
+
+window.addEventListener('load', dariKeyboard.init.bind(dariKeyboard));
